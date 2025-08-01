9458aea0bb64a11ad4a7e5c8a23a42dc659ecacb
Abduakhat Valiev <abduakhat.valiev@alif.tj>
2025-08-01 14:48:18 +0500

    added files

---
+++ reverse.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+    console.log(str.split(''))
+}
+
+splitString('hello')
+function reverseString(str) {
+    const arr = str.split('').reverse();
+    console.log(arr);
+}
+reverseString('hello')
+
+function joinString(str) {
+    console.log(str.split('').reverse().join(''))
+}
+joinString('hello')
